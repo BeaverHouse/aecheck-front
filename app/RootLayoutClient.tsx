ac53830a24b039eeef190c7d8350c2c5c631cc37
Austin Lee <haulrest@gmail.com>
2026-07-06 11:20:00 +0900

    chore: Change API spec, delete old logic (#72)

--- app/RootLayoutClient.tsx
+++ app/RootLayoutClient.tsx
@@ -4,7 +4,6 @@ import "@/i18n";
 import GlobalModal from "@/components/molecules/GlobalModal";
 import useModalStore from "@/store/useModalStore";
 import useConfigStore from "@/store/useConfigStore";
-import useCheckStore from "@/store/useCheckStore";
 import { ThemeOptions } from "@/constants/enum";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AECheckMenu from "@/components/organisms/Menu";
@@ -15,7 +14,6 @@ import { ArrowUp } from "lucide-react";
 import { useTranslation } from "react-i18next";
 import { useEffect } from "react";
 import Swal from "sweetalert2";
-import { getNumber } from "@/util/func";
 import { cn } from "@/lib/utils";
 import Footer from "@/components/atoms/Footer";
 
@@ -27,7 +25,6 @@ export default function RootLayoutClient({
   const { theme, colorBlindMode } = useConfigStore();
   const { modalType } = useModalStore();
   const { t } = useTranslation();
-  const { loadSaveData } = useCheckStore();
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -39,75 +36,6 @@ export default function RootLayoutClient({
       },
     },
   });
-
-  const migrate = async () => {
-    const oldData = window.localStorage.getItem("AE_CHECK");
-    if (!oldData || oldData.length < 1) return;
-    Swal.fire({
-      title: "Data Migration",
-      html: `<p style="font-size: 14px;">${t("frontend.message.migrate")}</p>`,
-      width: 300,
-      showCancelButton: true,
-    }).then(async (result) => {
-      if (result.isConfirmed) {
-        try {
-          const newData: CheckStateV4 = JSON.parse(oldData).state;
-          if (!newData.buddy) {
-            const charIds = newData.inven.map(
-              (i) => `char${String(i).padStart(4, "0")}`
-            );
-            const body = {
-              characterIds: charIds,
-            };
-            const res = await fetch(
-              `${process.env.NEXT_PUBLIC_API_URL}/buddy/partners`,
-              {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify(body),
-              }
-            );
-            const buddyList = (
-              (await res.json()) as APIResponse<IDInfo[]>
-            ).data.map((i) => getNumber(i));
-
-            newData.buddy = buddyList;
-          }
-          loadSaveData(newData);
-          Swal.fire({
-            text: "Data Load Success",
-            width: 280,
-            timer: 2000,
-            showConfirmButton: false,
-            timerProgressBar: true,
-            customClass: {
-              popup: "alert",
-            },
-          }).then(() => {
-            window.localStorage.removeItem("AE_CHECK");
-            window.location.reload();
-          });
-        } catch {
-          Swal.fire({
-            text: "Data Load Error",
-            width: 280,
-            timer: 1000,
-            showConfirmButton: false,
-            timerProgressBar: true,
-            customClass: {
-              popup: "alert",
-            },
-          });
-        }
-      }
-    });
-  };
-
-  useEffect(() => {
-    if (t("frontend.message.migrate") !== "frontend.message.migrate") migrate();
-  }, []);
 
   useEffect(() => {
     const timer = setTimeout(() => {
