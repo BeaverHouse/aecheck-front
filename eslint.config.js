06a23c07946f3784a8a505c8c3cfd15a15e80068
Austin Lee <haulrest@gmail.com>
2026-09-07 11:46:17 +0900

    fix: detect React version in ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -11,6 +11,9 @@ export default [
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
+    settings: {
+      react: { version: "detect" },
+    },
     rules: {
       "react/react-in-jsx-scope": "off",
       "react/prop-types": "off", // Using TypeScript instead
